1d5747a331709719c5db1266aff0d73b2b1a9712
DONALOMAOILCHIARAIN- <g00365724@atu.ie>
2024-09-30 10:16:29 +0100

    Add method

--- src/main/java/ie/atu/week3/Calc2024.java
+++ src/main/java/ie/atu/week3/Calc2024.java
@@ -12,16 +12,25 @@ public class Calc2024 {
         Scanner scan1 = new Scanner(System.in);
 
         System.out.println("Enter Number");
-        Double fristNum = scan1.nextDouble();
+        double fristNum = scan1.nextDouble();
         System.out.println("You Entered " + fristNum);
 
         System.out.println("Enter Second Number");
-        Double secondNum = scan1.nextDouble();
+        double secondNum = scan1.nextDouble();
         System.out.println("You Entered " + secondNum);
 
         System.out.println("Enter an operation (add, subtract, multiply, divide): ");
         String operation = scan1.next();
 
 
+        switch(operation){
+            case "add": add(fristNum, secondNum); break ; // pass the firstNum and secondNum from the scanner
+            default: System.out.println("Invalid operation");break;
+        }
+
+    }
+    public static void add(double a, double b){
+        double sum = a + b ;
+        System.out.println("Result is " + sum);
     }
 }
